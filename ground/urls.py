b4d521b8868f1df88e57fbab82a891aeec5ae73f
YvonneGi <giryvonne@gmail.com>
2019-11-06 15:30:54 +0200

    create vieux function to edit user profile

--- ground/urls.py
+++ ground/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
-   
+  url('^$',views.welcome,name = 'welcome'),
+  url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
+  url(r'^accounts/edit-profile/', views.edit_profile, name = 'edit-profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ground/views.py
+++ ground/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 import datetime as dt
 from playground import urls
 from .models import *
+from .forms import *
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -20,3 +21,18 @@ def profile(request,id):
     user = Profile.objects.get(username__id=id)
 
     return render(request, "profile.html", {"current_user":current_user,"user":user,"user_object":user_object})
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user=request.user
+    user_edit = Profile.objects.get(username__id=current_user.id)
+    if request.method =='POST':
+        form=ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+
+        return redirect("welcome")               
+    else:
+        form=ProfileForm(instance=request.user.profile)
+     
+    return render(request,'edit_profile.html',locals())
