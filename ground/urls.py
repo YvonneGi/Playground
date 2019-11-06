573758c38e6c36cb6e109b6c23ecedfc971c63e2
YvonneGi <giryvonne@gmail.com>
2019-11-06 12:22:01 +0200

    create templates and views function

---
+++ ground/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    # url('^today/$', views.display_image, name='Today'),
+    # # url(r'^photo/(\d+)', views.single_photo, name='singlePhoto'),
+    # url(r'^search/', views.search_images, name='search_images'),
+    # url(r'^all/$', views.all_images, name='allImages')
+   
+]
--- ground/views.py
+++ ground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+from playground import urls
+from .models import *
 
 # Create your views here.
+def welcome(request):
+    title='Playground'
+    return render(request, 'welcome.html',{title:'title'})
